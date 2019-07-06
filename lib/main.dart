82d60e3772d1e79cd5de58430bf20ca2d6a696db
Vivek <viveky259259@gmail.com>
2019-07-06 20:35:00 +0530

    dashboard    ui recreated

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: ProfileScreen(),
+      home: DashboardUi(),
       routes: {
         "/login": (context) => LoginUi(),
         "/dashboard": (context) => DashboardUi(),
